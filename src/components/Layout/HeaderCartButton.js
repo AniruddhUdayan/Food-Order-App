fa6c846b18f416d3e6f6fdc6580bfb7b08b683e3
Aniruddh Udayan <aniruddhudayan34@gmail.com>
2023-02-01 23:55:01 +0530

    made changes in header component

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,45 +1,42 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
-import CartIcon from '../Cart/CartIcon'
+import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
-
 export default function HeaderCartButton(props) {
-     const[btnIsHigh, setBtnIsHigh]=useState(false);
-    const cartCtx= useContext(CartContext);
+  const [btnIsHigh, setBtnIsHigh] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-    const{items} = cartCtx;
+  const { items } = cartCtx;
 
-    const numberOfCartItems = items.reduce((curNumber, item) => {
-        return curNumber + item.amount;
-    }, 0)
+  const numberOfCartItems = items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
-    const btnClasses = `${classes.button} ${btnIsHigh ? classes.bump : ''}`;
+  const btnClasses = `${classes.button} ${btnIsHigh ? classes.bump : ''}`;
 
-    useEffect(() => {
-        if(cartCtx.items.length === 0){
-            return;
-        }
-      setBtnIsHigh(true);
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHigh(true);
 
-     const timer= setTimeout(() => {
-          setBtnIsHigh(false)
-      }, 300);
+    const timer = setTimeout(() => {
+      setBtnIsHigh(false);
+    }, 300);
 
-      return()=> {
-          clearTimeout(timer);
-      };
-    },[items]);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items, cartCtx.items.length]);
 
-    return (
-        <button className={btnClasses} onClick={props.onClick}>
-            <span className={classes.icon}>
-              <CartIcon></CartIcon>
-            </span>
-            <span>Your Cart</span>
-            <span className={classes.badge}>
-                {numberOfCartItems}
-            </span>
-        </button>
-    )
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon></CartIcon>
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
 }
